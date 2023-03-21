e8a881996b9a07e2624a893c5a1d9b7817952a88
ezhzy3 <comin6@gmail.com>
2023-03-21 15:18:42 +0900

    23.03.21. 3pm

--- SERVICE FOLDER/rest/example/urls.py
+++ SERVICE FOLDER/rest/example/urls.py
@@ -1,8 +1,18 @@
-from django.urls import path
-from .views import HelloAPI, BookAPI, BooksAPI
+# from django.urls import path
+# from .views import *
 
-urlpatterns = [
-    path('hello/', HelloAPI),
-    path('books/',BooksAPI.as_view()),
-    path('book/<int:bid>/',BookAPI.as_view()),
-]
+# urlpatterns = [
+#     path('hello/', HelloAPI),
+#     path('books/',BooksAPI.as_view()),
+#     path('book/<int:bid>/',BookAPI.as_view()),
+#     path('mixin/books/',BooksAPIGenerics.as_view()),
+#     path('mixin/book/<int:bid>/',BookAPIGenerics.as_view()),
+# ]
+
+from rest_framework import routers
+from .views import BookViewSet
+
+router= routers.SimpleRouter()
+router.register('books',BookViewSet)
+
+urlpatterns=router.urls
