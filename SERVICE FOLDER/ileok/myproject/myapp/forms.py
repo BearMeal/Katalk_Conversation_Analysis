a2c8fa1b2cfe28551b56ae5351ff2e9f1dc6f8b1
ezhzy3 <comin6@gmail.com>
2023-05-02 13:50:31 +0900

    23.05.02 forms.py editing

--- SERVICE FOLDER/ileok/myproject/myapp/forms.py
+++ SERVICE FOLDER/ileok/myproject/myapp/forms.py
@@ -1,3 +1,17 @@
 from django import forms
+# class UploadFileForm(forms.Form):
+#     file = forms.FileField()
+
 class UploadFileForm(forms.Form):
+    ALLOWED_EXTENSIONS = ['txt']
     file = forms.FileField()
+
+    def clean_file(self):
+        file = self.cleaned_data.get('file')
+        if file:
+            filename = file.name
+            if not filename.endswith(tuple(self.ALLOWED_EXTENSIONS)):
+                raise forms.ValidationError('txt 파일만 업로드 가능합니다.')
+            if file.size > 1 * 1024 * 1024:
+                raise forms.ValidationError('1MB 이하의 파일만 업로드 가능합니다.')
+        return file
